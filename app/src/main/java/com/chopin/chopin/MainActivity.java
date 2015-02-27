ef99531ece4fa22b8354cdfbaf04fd3a9ad1b9bf
Branden Romero <brro5352@colorado.edu>
2015-02-27 15:53:56 -0700

    Modified song adapter

--- app/src/main/java/com/chopin/chopin/MainActivity.java
+++ app/src/main/java/com/chopin/chopin/MainActivity.java
@@ -6,19 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Main_menu extends Activity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_main);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
