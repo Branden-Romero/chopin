f92ae93c08539fae34e5cb1c5803037644e97767
mamc2567 <mamc2567@colorado.edu>
2015-03-11 20:07:24 -0600

    Test

--- Chopin/app/src/main/java/com/chopin/MainActivity.java
+++ Chopin/app/src/main/java/com/chopin/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends Activity implements MediaPlayerControl{
         songView.setAdapter(songAdt);
         setController();
     }
-//testing for push
+//testing2 for push
     private ServiceConnection musicConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
